c8bb0ce929d838543d194c824ffe0a7b27be363c
Aimee Barciauskas <abarciauskas@mdsol.com>
2016-02-14 19:27:06 +0100

    Fix ?

--- R/classify.R
+++ R/classify.R
@@ -2,7 +2,7 @@ library(randomForest)
 # ----------------------------------------------------------------------
 # Train randomForest model on training data
 # ----------------------------------------------------------------------
-#' randomForest.train
+#' train.randomForest
 #' 
 #' @param data.train matrix or data frame with training data
 #' @param label column name of the class identified with the row
@@ -11,8 +11,12 @@ library(randomForest)
 #' @return randomForest model trained on training data
 #' @export
 #'
-randomForest.train <- function(data.train, seed = 187, label = 'popularity') {
+#' @examples
+#' # train.randomForest(data.train)
+#' 
+train.randomForest <- function(data.train, seed = 187, label = 'popularity') {
   set.seed(seed)
   data.train[,label] <- factor(data.train[,label])
-  randomForest(x = data.train[,setdiff(colnames(data.train), label)], y = data.train[,label])
+  model <- randomForest(x = data.train[,setdiff(colnames(data.train), label)], y = data.train[,label])
+  return(model)
 }
