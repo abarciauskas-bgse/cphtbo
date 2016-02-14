5557a8ee474275f4d4cc2175aecb2457e95ef6b7
Aimee Barciauskas <abarciauskas@mdsol.com>
2016-02-14 19:06:38 +0100

    Update doc

--- R/classify.R
+++ R/classify.R
@@ -6,13 +6,12 @@ library(randomForest)
 #' 
 #' @param data.train matrix or data frame with training data
 #' @param label column name of the class identified with the row
-#' @param seed number to set as seed in case required for recreating exact results
+#' @param seed if required for recreating exact results
 #'
 #' @return randomForest model trained on training data
 #' @export
 #'
 #' @examples
-#' # model <- randomForest.train(data.train)
 #' 
 randomForest.train <- function(data.train, seed = 187, label = 'popularity') {
   set.seed(seed)
